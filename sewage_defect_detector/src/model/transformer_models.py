f12730abe764c3e5d223c525f8a9b8b9f06977c3
Sreejith Kannath Kalam <91134161+08-Sreejith-95@users.noreply.github.com>
2026-01-27 15:14:12 +0100

    key bug fixed

--- sewage_defect_detector/src/model/transformer_models.py
+++ sewage_defect_detector/src/model/transformer_models.py
@@ -1,19 +1,19 @@
 import timm
 import torch.nn as nn
 
-def build_vit_model(cfg):
+def build_vit_model(model_cfg):
     model = timm.create_model(
         "convnext_tiny",
         pretrained=True,
-        num_classes=cfg.num_classes
+        num_classes= model_cfg.num_classes
     )
     
     in_features = model.head.in_features
     model.head = nn.Sequential(
                                nn.LayerNorm(in_features), 
-                               nn.Linear(in_features, cfg.hidden_dim),
+                               nn.Linear(in_features, model_cfg.hidden_dim),
                                nn.GELU(),
-                               nn.Dropout(cfg.drop_out),
-                               nn.Linear(cfg.hidden_dim, cfg.num_classes)
+                               nn.Dropout(model_cfg.drop_out),
+                               nn.Linear(model_cfg.hidden_dim, model_cfg.num_classes)
                                )
     return model
