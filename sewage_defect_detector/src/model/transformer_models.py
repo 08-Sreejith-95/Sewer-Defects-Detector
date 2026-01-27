16d7bc38da457fcaa9130c0ff1192c3eafc2c982
Sreejith Kannath Kalam <91134161+08-Sreejith-95@users.noreply.github.com>
2026-01-27 15:48:13 +0100

    changed model head

--- sewage_defect_detector/src/model/transformer_models.py
+++ sewage_defect_detector/src/model/transformer_models.py
@@ -6,11 +6,12 @@ def build_vit_model(cfg):
     model = timm.create_model(
         "convnext_tiny",
         pretrained=True,
-        num_classes= cfg.dataset.num_classes,
+        num_classes= 0, # we'll replace the head,
     )
     
-    in_features = model.head.in_features
-    model.head = nn.Sequential(
+    in_features = model.num_features
+    model.head = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                               nn.Flatten(1),
                                nn.LayerNorm(in_features), 
                                nn.Linear(in_features, cfg.model.hidden_dim),
                                nn.GELU(),
